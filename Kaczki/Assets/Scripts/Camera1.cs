0442c7adbc4bcf917d04bcbaf7d157e2b87aa828
FilipGawenda <filipgawenda@wp.pl>
2022-11-09 20:20:16 +0100

    First commit

--- Kaczki/Assets/Scripts/Camera1.cs
+++ Kaczki/Assets/Scripts/Camera1.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camera1 : MonoBehaviour
 {
+    public GameObject cubeObject;
+    public Vector3 distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = cubeObject.transform.position + distance;
     }
 }
