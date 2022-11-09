0c78773f57462417319570d66ae4fd5e95af1e51
WakanaWill <magsla@outlook.com>
2022-11-09 20:39:01 +0100

    Revert "First commit"
    This reverts commit 0442c7adbc4bcf917d04bcbaf7d157e2b87aa828.

--- Kaczki/Assets/Scripts/Camera1.cs
+++ Kaczki/Assets/Scripts/Camera1.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera1 : MonoBehaviour
+public class Camera : MonoBehaviour
 {
-    public GameObject cubeObject;
-    public Vector3 distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +13,6 @@ public class Camera1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = cubeObject.transform.position + distance;
+        
     }
 }
